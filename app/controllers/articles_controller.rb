e732de6de8d02707037e146625dcad4034499bed
Barak Poker <bpoker@gmail.com>
2020-12-29 00:02:41 -0500

    Added updated abilities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,33 @@ end
 def index
   @articles = Article.all
 end
-  
+def new
+  @article = Article.new
+end
+def edit
+  @article = Article.find(params[:id])
+end
+
+def create
+  #render plain: params[:article]
+  @article = Article.new(params.require(:article).permit(:title, :description)) #whitelabeling what is permitted
+  #render plain: @article.inspect
+  if @article.save
+    flash[:notice] = "Article was created successfully."
+    redirect_to @article #redirect_to article_path(@article) #same as below
+  else
+    render 'new'
+  end
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(params.require(:article).permit(:title, :description))
+    flash[:notice] = "Article was updated successfully."
+    redirect_to @article #redirect_to article_path(@article) #same as below
+  else
+    render 'edit'
+  end
+end
+
 end
